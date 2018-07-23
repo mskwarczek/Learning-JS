bea98b80dcc717e6c3c7a2b7d661cf60fabb5f56
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-07-23 14:42:13 +0200

    change button click execution and add object containing all global variables

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 'use strict';
 
 // Global variables.
-var output = document.getElementById('output');
-var result = document.getElementById('result');
-var roundNumber = 0;
-var playerPoints = 0;
-var computerPoints = 0;
-var limitOfRounds = 0;
-var newGameActive = false;
-var playerName = 'Player';
+
+var params = {
+	output: document.getElementById('output'),
+	result: document.getElementById('result'),
+	roundNumber: 0,
+	playerPoints: 0,
+	computerPoints: 0,
+	limitOfRounds: 0,
+	newGameActive: false,
+	playerName: 'Player',
+	buttonSet: document.querySelectorAll('.player-move')
+}
+
+// Add event listeners to buttons.
+for (var i = 0; i < params.buttonSet.length; i++) {
+	params.buttonSet[i].addEventListener('click', playerMove);
+	params.buttonSet[i].value = params.buttonSet[i].getAttribute('data-move');
+}
 
 // Write additional text at the end of div with id="output".
 function writeText (text) {
-	output.insertAdjacentHTML('beforeend', text);
+	params.output.insertAdjacentHTML('beforeend', text);
 }
 
 // Randomly choose computer's move and return it to playerMove function.
@@ -52,7 +62,7 @@ function giveRoundResult (playMove, compMove) {
 						[-1, 0, 1],
 						[1, -1, 0]];
 	var roundResult = valuesMatrix[setIndex(compMove)][setIndex(playMove)];
-	writeText(playerName + ' played ' + playMove + ', computer played ' + compMove + '... ');
+	writeText(params.playerName + ' played ' + playMove + ', computer played ' + compMove + '... ');
 	switch (roundResult) {
 		case -1: writeText('You lost!<br>'); break;
 		case 0: writeText('A draw!<br>'); break;
@@ -63,31 +73,31 @@ function giveRoundResult (playMove, compMove) {
 
 // Check for winner of the entire game.
 function gameEnd () {
-	if (playerPoints === limitOfRounds) {
-		writeText(playerName + ' won the the entire game!<br>');
-		newGameActive = false;
+	if (params.playerPoints === params.limitOfRounds) {
+		writeText(params.playerName + ' won the the entire game!<br>');
+		params.newGameActive = false;
 	}
-	if (computerPoints === limitOfRounds) {
+	if (params.computerPoints === params.limitOfRounds) {
 		writeText('Computer won the entire game!<br>');
-		newGameActive = false;
+		params.newGameActive = false;
 	}
 }
 
 // Count current round number and points of each player.
 function countPoints (roundResult) {
 	switch (roundResult) {
-		case -1: ++computerPoints; break;
+		case -1: ++params.computerPoints; break;
 		case 0: break;
-		case 1: ++playerPoints; break;
+		case 1: ++params.playerPoints; break;
 	}
-	++roundNumber;
-	result.innerHTML = '<br>Round: ' + roundNumber + ' Rounds to win: ' + limitOfRounds + '<br>' + playerName + ' ' + playerPoints + ' - ' + computerPoints + ' Computer';
+	++params.roundNumber;
+	result.innerHTML = '<br>Round: ' + params.roundNumber + ' Rounds to win: ' + params.limitOfRounds + '<br>' + params.playerName + ' ' + params.playerPoints + ' - ' + params.computerPoints + ' Computer';
 }
 
 // Begin new round of game.
-function playerMove (button) {
-	if (newGameActive) {
-		var playMove = button.id;
+function playerMove (dataMove) {
+	if (params.newGameActive) {
+		var playMove = dataMove.target.value;
 		var compMove = computerMove();
 		var roundResult = giveRoundResult(playMove, compMove);
 		countPoints(roundResult);
@@ -100,19 +110,19 @@ function playerMove (button) {
 
 // Clear output and result sections.
 function clearText () {
-	output.innerHTML = "";
-	result.innerHTML = "";
+	params.output.innerHTML = "";
+	params.result.innerHTML = "";
 }
 
 // Start a new game.
 function newGame () {
 	clearText();
-	roundNumber = 0;
-	playerPoints = 0;
-	computerPoints = 0;
-	limitOfRounds = 0;
-	playerName = window.prompt('Player name: ');
-	limitOfRounds = window.prompt('Total number of wins needed to win the entire game: ');
-	limitOfRounds = parseInt(limitOfRounds);
-	newGameActive = true;
+	params.roundNumber = 0;
+	params.playerPoints = 0;
+	params.computerPoints = 0;
+	params.limitOfRounds = 0;
+	params.playerName = window.prompt('Player name: ');
+	params.limitOfRounds = window.prompt('Total number of wins needed to win the entire game: ');
+	params.limitOfRounds = parseInt(params.limitOfRounds);
+	params.newGameActive = true;
 }
