4f910253d7c381406500d27863a71f5a25a76072
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-07-24 15:05:35 +0200

    add carousel

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
+	hash: true,
+	pageDots: false,
+	cellAlign: 'left',
+	contain: true
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-  // options
+var restartButton = document.querySelector('.button-restart');
+restartButton.addEventListener('click', function() {
+	flkty.select(0)
+});
+
+var progressBar = document.querySelector('.progress-bar')
+flkty.on('scroll', function(progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + '%';
 });
