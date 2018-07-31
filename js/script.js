1d4e6a0ec3750b66d4a3b688b0d45fac092f9172
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-07-31 14:15:52 +0200

    Modify getJoke function to use axios

--- js/script.js
+++ js/script.js
@@ -10,13 +10,9 @@ button.addEventListener('click', function(){
 var paragraph = document.getElementById('joke');
 
 function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
-}
+	axios.get(url)
+	.then(function (response) {
+    	paragraph.innerHTML = response.data.value.joke;
+})}
 
 getJoke();
