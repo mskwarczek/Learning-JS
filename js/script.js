c85b3cf3ee25ee9f571bbec642d9adb76bfff90b
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-08-01 17:46:39 +0200

    add css styles

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function createTweet(input) {
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    var tweetText = "Quote of the day - " + quoteText + ". Author: " + quoteAuthor;
     if (tweetText.length > 140) {
         getQuote();
     } else {
