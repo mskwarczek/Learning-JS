0d281ecd17d3e8eeed41de1a609075924404a21c
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-08-03 09:50:47 +0200

    add basic API communication

---
+++ js/App.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '3410',
+  'X-Auth-Token': 'd2f5e319b6dbf5f4eaf6358aebb5886f',
+  'Content-Type': 'application/json; charset=utf-8'
+};
+
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+});
+
+function setupColumns(columns) {
+  columns.forEach(function (column) {
+    var col = new Column(column.id, column.name);
+    board.addColumn(col);
+    setupCards(col, column.cards);
+  });
+}
+
+function setupCards(col, cards) {
+  cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+    col.addCard(cardObj);
+  });
+}
+
+function generateTemplate(name, data, basicElement) {
+  var template = document.getElementById(name).innerHTML;
+  var element = document.createElement(basicElement || 'div');
+	Mustache.parse(template);
+  	element.innerHTML = Mustache.render(template, data);
+	return element;
+}
