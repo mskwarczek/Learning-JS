08eb985192fd2fbd43239928f18594e7ae92a074
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-08-14 16:41:22 +0200

    add js code and styles

---
+++ js/components/Contact.js
@@ -0,0 +1,20 @@
+var Contact = React.createClass({
+    propTypes: {
+        item: React.PropTypes.object.isRequired,
+    },
+  
+    render: function() {
+        return (
+            React.createElement('div', {className: 'contactItem'},
+                React.createElement('img', {
+                    className: 'contactImage',
+                    src: 'http://icons.veryicon.com/png/System/Qetto%202/contacts.png'
+                }),
+                React.createElement('p', {className: 'contactFirstName'}, 'Imię: ' + this.props.item.firstName),
+                React.createElement('p', {className: 'contactLastName'}, 'Nazwisko: ' + this.props.item.lastName),
+                React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+                this.props.item.email)
+            )
+        )
+    },
+});
