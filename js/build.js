3e092830cc83b5f7c508031be56d614c5a65046f
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-09-01 14:03:13 +0200

    fix bugs

--- js/build.js
+++ js/build.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -19,8 +19,6 @@ var Stopwatch = function (_React$Component) {
         _this.state = {
             id: new Date().getUTCMilliseconds(),
             running: false,
-            display: null,
-            results: null,
             resultsArray: [],
             times: {
                 minutes: 0,
@@ -32,156 +30,132 @@ var Stopwatch = function (_React$Component) {
     }
 
     _createClass(Stopwatch, [{
-        key: 'componentDidMount',
-        value: function componentDidMount() {
-            var _this2 = this;
-
-            this.setState({
-                display: document.getElementById('display-' + this.state.id),
-                results: document.getElementById('results-' + this.state.id)
-            });
-
-            var startButton = document.getElementById('start-' + this.state.id);
-            startButton.addEventListener('click', function () {
-                return _this2.start();
-            });
-
-            var stopButton = document.getElementById('stop-' + this.state.id);
-            stopButton.addEventListener('click', function () {
-                return _this2.stop();
-            });
-
-            var resetButton = document.getElementById('reset-' + this.state.id);
-            resetButton.addEventListener('click', function () {
-                return _this2.reset();
-            });
-
-            var splitButton = document.getElementById('split-' + this.state.id);
-            splitButton.addEventListener('click', function () {
-                return _this2.split();
-            });
-
-            var clearButton = document.getElementById('clear-' + this.state.id);
-            clearButton.addEventListener('click', function () {
-                return _this2.clear();
-            });
+        key: "pad0",
+        value: function pad0(value) {
+            var result = value.toString();
+            if (result.length < 2) {
+                result = '0' + result;
+            }
+            return result;
         }
     }, {
-        key: 'setTime0',
-        value: function setTime0() {
-            this.state.times = {
-                minutes: 0,
-                seconds: 0,
-                miliseconds: 0
-            };
-        }
-    }, {
-        key: 'print',
-        value: function print() {
-            updateStateDisplay(this.state.times);
-        }
-    }, {
-        key: 'start',
+        key: "start",
         value: function start() {
-            var _this3 = this;
+            var _this2 = this;
 
             if (!this.state.running) {
                 this.setState({ running: true });
                 this.watch = setInterval(function () {
-                    return _this3.step();
+                    return _this2.calculate();
                 }, 10);
-                this.state.display.classList.add('running');
             }
         }
     }, {
-        key: 'step',
-        value: function step() {
-            if (!this.state.running) return;
-            this.calculate();
-            this.print();
-        }
-    }, {
-        key: 'calculate',
+        key: "calculate",
         value: function calculate() {
-            this.state.times.miliseconds += 1;
-            if (this.state.times.miliseconds >= 100) {
-                this.state.times.seconds += 1;
-                this.state.times.miliseconds = 0;
-            }
-            if (this.state.times.seconds >= 60) {
-                this.state.times.minutes += 1;
-                this.state.times.seconds = 0;
-            }
+            if (!this.state.running) return;
+            this.setState(function () {
+                this.state.times.miliseconds += 1;
+                if (this.state.times.miliseconds >= 100) {
+                    this.state.times.seconds += 1;
+                    this.state.times.miliseconds = 0;
+                }
+                if (this.state.times.seconds >= 60) {
+                    this.state.times.minutes += 1;
+                    this.state.times.seconds = 0;
+                }
+            });
         }
     }, {
-        key: 'stop',
+        key: "stop",
         value: function stop() {
             this.setState({
                 running: false
             });
             clearInterval(this.watch);
-            this.state.display.classList.remove('running');
         }
     }, {
-        key: 'reset',
+        key: "reset",
         value: function reset() {
             if (this.state.running) this.stop();
-            this.setTime0();
-            this.print();
+            this.setState({
+                times: {
+                    miliseconds: 0,
+                    seconds: 0,
+                    minutes: 0
+                }
+            });
         }
     }, {
-        key: 'split',
+        key: "split",
         value: function split() {
             this.setState(function () {
-                return this.state.resultsArray = this.state.resultsArray.push(pad0(this.state.times.minutes) + ':' + pad0(this.state.times.seconds) + ':' + pad0(Math.floor(this.state.times.miliseconds)));
+                this.state.resultsArray = this.state.resultsArray.push(this.pad0(this.state.times.minutes) + ":" + this.pad0(this.state.times.seconds) + ":" + this.pad0(Math.floor(this.state.times.miliseconds)));
             });
-            updateStateResults(this.state.resultsArray);
         }
     }, {
-        key: 'clear',
+        key: "clear",
         value: function clear() {
             this.setState({
                 resultsArray: []
             });
-            updateStateResults(this.state.resultsArray);
         }
     }, {
-        key: 'render',
+        key: "render",
         value: function render() {
+            var _this3 = this;
+
             return React.createElement(
-                'div',
+                "div",
                 null,
                 React.createElement(
-                    'div',
-                    { className: 'controls' },
+                    "div",
+                    { className: "controls" },
                     React.createElement(
-                        'a',
-                        { href: '#', className: 'button', id: "start-" + this.state.id },
-                        'Start'
+                        "a",
+                        { href: "#", className: "button", onClick: function onClick() {
+                                return _this3.start();
+                            }, id: "start-" + this.state.id },
+                        "Start"
                     ),
                     React.createElement(
-                        'a',
-                        { href: '#', className: 'button', id: "stop-" + this.state.id },
-                        'Stop'
+                        "a",
+                        { href: "#", className: "button", onClick: function onClick() {
+                                return _this3.stop();
+                            }, id: "stop-" + this.state.id },
+                        "Stop"
                     ),
                     React.createElement(
-                        'a',
-                        { href: '#', className: 'button', id: "reset-" + this.state.id },
-                        'Reset'
+                        "a",
+                        { href: "#", className: "button", onClick: function onClick() {
+                                return _this3.reset();
+                            }, id: "reset-" + this.state.id },
+                        "Reset"
                     ),
                     React.createElement(
-                        'a',
-                        { href: '#', className: 'button', id: "split-" + this.state.id },
-                        'Split'
+                        "a",
+                        { href: "#", className: "button", onClick: function onClick() {
+                                return _this3.split();
+                            }, id: "split-" + this.state.id },
+                        "Split"
                     ),
                     React.createElement(
-                        'a',
-                        { href: '#', className: 'button', id: "clear-" + this.state.id },
-                        'Clear'
+                        "a",
+                        { href: "#", className: "button", onClick: function onClick() {
+                                return _this3.clear();
+                            }, id: "clear-" + this.state.id },
+                        "Clear"
                     )
                 ),
-                React.createElement(Display, { times: this.state.times, stopwatchID: this.state.id }),
-                React.createElement(Results, { stopwatchID: this.state.id })
+                React.createElement(Display, {
+                    stopwatchID: this.state.id,
+                    time: this.pad0(this.state.times.minutes) + ":" + this.pad0(this.state.times.seconds) + ":" + this.pad0(Math.floor(this.state.times.miliseconds)),
+                    running: this.state.running
+                }),
+                React.createElement(Results, {
+                    stopwatchID: this.state.id,
+                    resultsArray: this.state.resultsArray
+                })
             );
         }
     }]);
@@ -189,103 +163,34 @@ var Stopwatch = function (_React$Component) {
     return Stopwatch;
 }(React.Component);
 
-var Display = function (_React$Component2) {
-    _inherits(Display, _React$Component2);
+var Display = function Display(props) {
+    return React.createElement(
+        "div",
+        { className: "display" + (props.running ? " running" : ""), id: "display-" + props.stopwatchID },
+        React.createElement(
+            "p",
+            null,
+            props.time
+        )
+    );
+};
 
-    function Display(props) {
-        _classCallCheck(this, Display);
-
-        var _this4 = _possibleConstructorReturn(this, (Display.__proto__ || Object.getPrototypeOf(Display)).call(this, props));
-
-        _this4.state = {
-            stopwatchID: _this4.props.stopwatchID,
-            times: {
-                minutes: 0,
-                seconds: 0,
-                miliseconds: 0
-            }
-        };
-        updateStateDisplay = updateStateDisplay.bind(_this4);
-        return _this4;
-    }
-
-    _createClass(Display, [{
-        key: 'render',
-        value: function render() {
-            return React.createElement(
-                'div',
-                { className: 'display', id: "display-" + this.state.stopwatchID },
-                React.createElement(
-                    'p',
-                    null,
-                    pad0(this.state.times.minutes),
-                    ':',
-                    pad0(this.state.times.seconds),
-                    ':',
-                    pad0(Math.floor(this.state.times.miliseconds))
-                )
-            );
-        }
-    }]);
-
-    return Display;
-}(React.Component);
-
-var Results = function (_React$Component3) {
-    _inherits(Results, _React$Component3);
-
-    function Results(props) {
-        _classCallCheck(this, Results);
-
-        var _this5 = _possibleConstructorReturn(this, (Results.__proto__ || Object.getPrototypeOf(Results)).call(this, props));
-
-        _this5.state = {
-            stopwatchID: _this5.props.stopwatchID,
-            resultsArray: []
-        };
-        updateStateResults = updateStateResults.bind(_this5);
-        return _this5;
-    }
-
-    _createClass(Results, [{
-        key: 'render',
-        value: function render() {
-            var listResults = this.state.resultsArray.map(function (result, index) {
+var Results = function Results(props) {
+    return React.createElement(
+        "div",
+        { className: "results", id: "results-" + props.stopwatchID },
+        React.createElement(
+            "ol",
+            null,
+            props.resultsArray.map(function (result, index) {
                 return React.createElement(
-                    'li',
+                    "li",
                     { key: index },
                     result
                 );
-            });
-            return React.createElement(
-                'div',
-                { className: 'results', id: "results-" + this.state.stopwatchID },
-                React.createElement(
-                    'ol',
-                    null,
-                    listResults
-                )
-            );
-        }
-    }]);
-
-    return Results;
-}(React.Component);
-
-function pad0(value) {
-    var result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
-}
-
-function updateStateDisplay(times) {
-    this.setState({ times: times });
-}
-
-function updateStateResults(resultsArray) {
-    this.setState({ resultsArray: resultsArray });
-}
+            })
+        )
+    );
+};
 
 ReactDOM.render(React.createElement(Stopwatch, null), document.querySelector('.app'));
--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ class Stopwatch extends React.Component {
         this.state = {
             id: new Date().getUTCMilliseconds(),
             running: false,
-            display: null,
-            results: null,
             resultsArray: [],
             times: {
                 minutes: 0,
@@ -16,64 +14,34 @@ class Stopwatch extends React.Component {
         };
     }
 
-    componentDidMount() {
-        this.setState({
-            display: document.getElementById(`display-${this.state.id}`),
-            results: document.getElementById(`results-${this.state.id}`)
-        })
-
-        let startButton = document.getElementById(`start-${this.state.id}`);
-        startButton.addEventListener('click', () => this.start());
-
-        let stopButton = document.getElementById(`stop-${this.state.id}`);
-        stopButton.addEventListener('click', () => this.stop());
-
-        let resetButton = document.getElementById(`reset-${this.state.id}`);
-        resetButton.addEventListener('click', () => this.reset());
-
-        let splitButton = document.getElementById(`split-${this.state.id}`);
-        splitButton.addEventListener('click', () => this.split());
-
-        let clearButton = document.getElementById(`clear-${this.state.id}`);
-        clearButton.addEventListener('click', () => this.clear());
-    }
-
-    setTime0() {
-        this.state.times = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-        };
-    }
-
-    print() {
-        updateStateDisplay(this.state.times);
+    pad0(value) {
+        let result = value.toString();
+        if (result.length < 2) {
+            result = '0' + result;
+        }
+        return result;
     }
 
     start() {
         if (!this.state.running) {
             this.setState({running: true});
-            this.watch = setInterval(() => this.step(), 10);
-            this.state.display.classList.add('running');
+            this.watch = setInterval(() => this.calculate(), 10);
         }
-    }
-
-    step() {
-        if (!this.state.running) return;
-        this.calculate();
-        this.print();
     }
 
     calculate() {
-        this.state.times.miliseconds += 1;
-        if (this.state.times.miliseconds >= 100) {
-            this.state.times.seconds += 1;
-            this.state.times.miliseconds = 0;
-        }
-        if (this.state.times.seconds >= 60) {
-            this.state.times.minutes += 1;
-            this.state.times.seconds = 0;
-        }
+        if (!this.state.running) return;
+        this.setState(function() {
+            this.state.times.miliseconds += 1;
+            if (this.state.times.miliseconds >= 100) {
+                this.state.times.seconds += 1;
+                this.state.times.miliseconds = 0;
+            }
+            if (this.state.times.seconds >= 60) {
+                this.state.times.minutes += 1;
+                this.state.times.seconds = 0;
+            }
+        });
     }
 
     stop() {
@@ -81,108 +49,67 @@ class Stopwatch extends React.Component {
             running: false
         });
         clearInterval(this.watch);
-        this.state.display.classList.remove('running');
     }
 
     reset() {
         if(this.state.running) this.stop();
-        this.setTime0();
-        this.print();
+        this.setState({
+            times: {
+                miliseconds: 0,
+                seconds: 0,
+                minutes: 0
+            }
+        });
     }
 
     split() {
         this.setState(function() {
-            return this.state.resultsArray = this.state.resultsArray.push(`${pad0(this.state.times.minutes)}:${pad0(this.state.times.seconds)}:${pad0(Math.floor(this.state.times.miliseconds))}`)
+            this.state.resultsArray = this.state.resultsArray.push(`${this.pad0(this.state.times.minutes)}:${this.pad0(this.state.times.seconds)}:${this.pad0(Math.floor(this.state.times.miliseconds))}`)
         });
-        updateStateResults(this.state.resultsArray);
     }
 
     clear() {
         this.setState({
             resultsArray: []
         })
-        updateStateResults(this.state.resultsArray);
     }
 
     render() {
         return (
             <div>
                 <div className="controls">
-                    <a href="#" className="button" id={"start-" + this.state.id}>Start</a>
-                    <a href="#" className="button" id={"stop-" + this.state.id}>Stop</a>
-                    <a href="#" className="button" id={"reset-" + this.state.id}>Reset</a>
-                    <a href="#" className="button" id={"split-" + this.state.id}>Split</a>
-                    <a href="#" className="button" id={"clear-" + this.state.id}>Clear</a>
+                    <a href="#" className="button" onClick={() => this.start()} id={"start-" + this.state.id}>Start</a>
+                    <a href="#" className="button" onClick={() => this.stop()} id={"stop-" + this.state.id}>Stop</a>
+                    <a href="#" className="button" onClick={() => this.reset()} id={"reset-" + this.state.id}>Reset</a>
+                    <a href="#" className="button" onClick={() => this.split()} id={"split-" + this.state.id}>Split</a>
+                    <a href="#" className="button" onClick={() => this.clear()} id={"clear-" + this.state.id}>Clear</a>
                 </div>
-                <Display times={this.state.times} stopwatchID={this.state.id}/>
-                <Results stopwatchID={this.state.id}/>
+                <Display
+                    stopwatchID={this.state.id}
+                    time={`${this.pad0(this.state.times.minutes)}:${this.pad0(this.state.times.seconds)}:${this.pad0(Math.floor(this.state.times.miliseconds))}`}
+                    running={this.state.running}
+                />
+                <Results
+                    stopwatchID={this.state.id}
+                    resultsArray={this.state.resultsArray}
+                />
             </div>
         );
     }
 }
 
-class Display extends React.Component {
+const Display = props =>
+    <div className={`display${props.running ? " running" : ""}`} id={"display-" + props.stopwatchID}>
+        <p>{props.time}</p>
+    </div>
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            stopwatchID: this.props.stopwatchID,
-            times: {
-                minutes: 0,
-                seconds: 0,
-                miliseconds: 0
-            }
-        }
-        updateStateDisplay = updateStateDisplay.bind(this)
-    }
-    render() {
-        return (
-            <div className="display" id={"display-" + this.state.stopwatchID}>
-                <p>{pad0(this.state.times.minutes)}:{pad0(this.state.times.seconds)}:{pad0(Math.floor(this.state.times.miliseconds))}</p>
-            </div>
-        );
-    }
-}
-
-class Results extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            stopwatchID: this.props.stopwatchID,
-            resultsArray: []
-        }
-        updateStateResults = updateStateResults.bind(this)
-    }
-
-    render() {
-        let listResults = this.state.resultsArray.map((result, index) => 
-            <li key={index}>{result}</li>);
-        return (
-            <div className="results" id={"results-" + this.state.stopwatchID}>
-            <ol>
-                {listResults}
-            </ol>
-            </div>
-        );
-    }
-}
-
-function pad0(value) {
-    let result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
-}
-
-function updateStateDisplay(times){
-    this.setState({times});
-}
-
-function updateStateResults(resultsArray){
-    this.setState({resultsArray});
-}
+const Results = props =>
+    <div className="results" id={"results-" + props.stopwatchID}>
+        <ol>
+            {props.resultsArray.map((result, index) => 
+            <li key={index}>{result}</li>)}
+        </ol>
+    </div>
 
 ReactDOM.render(
     <Stopwatch />,
