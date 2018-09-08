ad9a2ddce63f224ce07ccd6df85fa9a3109b864e
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-09-08 08:33:10 +0200

    add endpoint /store and middleware functions

--- server.js
+++ server.js
@@ -15,8 +15,21 @@ app.get('/userform', function (req, res) {
     res.end(JSON.stringify(response));
 });
 
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep.');
+});
+
 var server = app.listen(3000, 'localhost', function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Aaplikacja nasłuchuje na http://' + host + ':' + port);
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
