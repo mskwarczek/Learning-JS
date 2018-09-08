9124a00e04fa271ab811c38a8901661f84c8957c
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-09-08 09:20:57 +0200

    add pug templates

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('assets'));
+app.set('view engine', 'pug');
+app.set('views','./views');
 
-app.get('/', function (req, res) {
-    res.sendFile('/index.html');
+app.get('/', function(req, res){
+    res.render('index');
 });
 
-app.get('/userform', function (req, res) {
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.end(JSON.stringify(response));
-});
-
-app.use('/store', function(req, res, next){
-    console.log('Jestem pośrednikiem przy żądaniu do /store');
-    next();
-});
-
-app.get('/store', function (req, res) {
-    res.send('To jest sklep.');
+app.get('/auth/google', function(req, res){
+    res.render('content');
 });
 
 var server = app.listen(3000, 'localhost', function() {
@@ -31,5 +19,5 @@ var server = app.listen(3000, 'localhost', function() {
 });
 
 app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+    res.status(404).send('Error 404. Page not found :(');
 });
