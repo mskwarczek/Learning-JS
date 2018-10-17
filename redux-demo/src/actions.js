9bf9aaa4ed39445839f1c09bc1ef2c7491d59b63
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-10-17 10:54:26 +0200

    add basic structure and fix bugs

---
+++ redux-demo/src/actions.js
@@ -0,0 +1,56 @@
+import uuid from 'uuid';
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    console.log(`inside addComment, text: ${text}`);
+    return {
+        type: ADD_COMMENT,
+        id: uuid.v4(),
+        text
+    }
+}
+
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text
+    }
+}
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id
+    }
+}
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id
+    }
+}
+
+export {ADD_COMMENT};
+export {REMOVE_COMMENT};
+export {EDIT_COMMENT};
+export {THUMB_UP_COMMENT};
+export {THUMB_DOWN_COMMENT};
+export {addComment};
+export {removeComment};
+export {editComment};
+export {thumbUpComment};
+export {thumbDownComment};
