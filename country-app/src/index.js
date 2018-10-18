e534606642a110327314cc291553aa3f8aa485a4
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-10-18 10:24:28 +0200

    add routing and navigation

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
 import store from './store';
+import routes from './routes';
 import DevTools from './DevTools';
-
-import { getCountries } from './actions/actions-countries';
 
 render(
     <Provider store={store}>
         <div>
-            <h1>Inicjalizacja projektu</h1>
+            <Router history={hashHistory} routes={routes}/>
             <DevTools />
         </div>
     </Provider>,
     document.getElementById('root')
 );
-
-store.dispatch(getCountries());
