33a1978d31687190eee0b052e276b935f42106cd
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-10-25 15:42:05 +0200

    add new functions

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/nodeappdatabase'
-//, {    useMongoClient: true}
-);
+mongoose.connect('mongodb://testUser:testpassword1@ds115592.mlab.com:15592/nodeappdatabase', {useNewUrlParser: true});
 
 const userSchema = new Schema({
     name: String,
@@ -38,14 +36,13 @@ const kenny = new User({
     password: 'password'
 });
 
-kenny.manify(function(err, name) {
+kenny.manify(function (err, name) {
     if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
 });
 
-kenny.save(function(err) {
+kenny.save(function (err) {
     if (err) throw err;
-    console.log('Uzytkownik zapisany pomyslnie');
+    console.log('Uzytkownik ' + kenny.name + ' zapisany pomyslnie');
 });
 
 const benny = new User({
@@ -54,14 +51,13 @@ const benny = new User({
     password: 'password'
 });
 
-benny.manify(function(err, name) {
+benny.manify(function (err, name) {
     if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
 });
 
-benny.save(function(err) {
+benny.save(function (err) {
     if (err) throw err;
-    console.log('Uzytkownik ' + benny.name +  ' zapisany pomyslnie');
+    console.log('Uzytkownik ' + benny.name + ' zapisany pomyslnie');
 });
 
 const mark = new User({
@@ -70,39 +66,35 @@ const mark = new User({
     password: 'password'
 });
 
-mark.manify(function(err, name) {
+mark.manify(function (err, name) {
     if (err) throw err;
-    console.log('Twoje nowe imię to: ' + name);
 });
 
-mark.save(function(err) {
+mark.save(function (err) {
     if (err) throw err;
-    console.log('Uzytkownik ' + mark.name +  ' zapisany pomyslnie');
+    console.log('Uzytkownik ' + mark.name + ' zapisany pomyslnie');
 });
 
 const findAllUsers = function() {
-    // find all users
     return User.find({}, function(err, res) {
         if (err) throw err;
-        console.log('Actual database records are ' + res);
+        console.log('Actual database records are: ' + res);
     });
 }
 
-const findSpecificRecord = function() {
-    // find specific record
-    return User.find({ username: 'Kenny_the_boy' }, function(err, res) {
+const findSpecificRecord = function(param) {
+    return User.find({ username: param }, function(err, res) {
         if (err) throw err;
-        console.log('Record you are looking for is ' + res);
+        console.log('Record you are looking for is:\n' + res);
     })
 }
 
-const updadeUserPassword = function() {
-    // update user password
-    return User.findOne({ username: 'Kenny_the_boy' })
+const updadeUserPassword = function(param1, param2) {
+    return User.findOne({ username: param1 })
         .then(function(user) {
             console.log('Old password is ' + user.password);
             console.log('Name ' + user.name);
-            user.password = 'newPassword';
+            user.password = param2;
             console.log('New password is ' + user.password);
             return user.save(function(err) {
                 if (err) throw err;
@@ -111,27 +103,15 @@ const updadeUserPassword = function() {
         })
 }
 
-const updateUsername = function() {
-    // update username
-    return User.findOneAndUpdate({ username: 'Benny_the_boy' }, { username: 'Benny_the_man' }, { new: true }, function(err, user) {
+const updateUsername = function(param1, param2) {
+    return User.findOneAndUpdate({ username: param1 }, { username: param2 }, { new: true }, function(err, user) {
         if (err) throw err;
         console.log('Nazwa uzytkownika po aktualizacji to ' + user.username);
     })
 }
 
-const findMarkAndDelete = function() {
-    // find specific user and delete
-    return User.findOne({ username: 'Mark_the_boy' })
-        .then(function(user) {
-            return user.remove(function() {
-                console.log('User successfully deleted');
-            });
-        })
-}
-
-const findKennyAndDelete = function() {
-    // find specific user and delete
-    return User.findOne({ username: 'Kenny_the_boy' })
+const removeUser = function(param) {
+    return User.findOneAndRemove({ username: param })
         .then(function(user) {
             return user.remove(function() {
                 console.log('User successfully deleted');
@@ -139,22 +119,35 @@ const findKennyAndDelete = function() {
         });
 }
 
-const findBennyAndRemove = function() {
-    // find specific user and delete
-    return User.findOneAndRemove({ username: 'Benny_the_man' })
-        .then(function(user) {
-            return user.remove(function() {
-                console.log('User successfully deleted');
-            });
-        });
+const removeAll = function() {
+    return User.deleteMany({}, function(err) {
+        if (err) throw err;
+        console.log('All users have been removed')
+    });
 }
 
-Promise.all([kenny.save(), mark.save(), benny.save()])
-    .then(findAllUsers)
-    .then(findSpecificRecord)
-    .then(updadeUserPassword)
-    .then(updateUsername)
-    .then(findMarkAndDelete)
-    .then(findKennyAndDelete)
-    .then(findBennyAndRemove)
-    .catch(console.log.bind(console))
+process.stdin.setEncoding('utf-8');
+console.log(`
+Commands:
+/findAll (show all users)
+/findUser <userName> (example: /find Kenny_the_boy)
+/changePassword <userName> <newPassword> (example: /changePassword Kenny_the_boy qwe123)
+/changeName <userName> <newName> (example: /changeName Kenny_the_boy Kenny)
+/removeUser <userName> (example: /removeUser Kenny_the_boy)
+/removeAll (removes ALL users from the collection\n`)
+
+process.stdin.on('readable', function() {
+    let input = process.stdin.read();
+    if (input !== null) {
+        let instruction = input.toString().trim().split(' ');
+        switch (instruction[0]) {
+            case '/findAll': findAllUsers(); break;
+            case '/findUser': findSpecificRecord(instruction[1]); break;
+            case '/changePassword': updadeUserPassword(instruction[1], instruction[2]); break;
+            case '/changeName': updateUsername(instruction[1], instruction[2]); break;
+            case '/removeUser': removeUser(instruction[1]); break;
+            case '/removeAll': removeAll(); break;
+            default: console.log('Wrong instruction!\n');
+        }
+    }
+});
